019ff059e5535f31b6616c68ef0eba30ffc6cfeb
Sarthak Tanwar <sarthaktanwar50@gmail.com>
2022-03-15 17:07:31 +0530

    Added some comments to FirstModule.c

--- FirstModule.c
+++ FirstModule.c
@@ -1,22 +1,27 @@
+//including the header files (they're different from normal C stuff)
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+//specifying details about the module
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sarthak");
 MODULE_DESCRIPTION("First kernel module idk what I'm doing");
 MODULE_VERSION("1");
 
+//when module is pushed into the kernel
 static int __init hello(void)
 {
 	printk(KERN_INFO "Hello world!\n");
 	return 0;
 }
 
+//message for when the module is taken out from the kernel
 static void __exit getMeOuttaHere(void)
 {
 	printk(KERN_INFO "Bye bye!\n");
 }
 
+//these 2 functions are required
 module_init(hello);
 module_exit(getMeOuttaHere);
